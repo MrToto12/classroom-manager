78e7beb12272bf8c4580dd59364fada47926a949
MrToto12 <tototemporelli12@gmail.com>
2023-11-15 18:14:39 -0300

    En esta rama voy a intentar reemplazar el AbstractFactory de los cursos por otro factory method, porque el abstract factory esta mal implementado y no cumple ninguna funcion en el dominio del problema.

--- src/Main.java
+++ src/Main.java
@@ -103,6 +103,7 @@ public class Main {
                 case 7:
                     // Acciones con un docente (asignar curso, listar cursos, etc)
                     // accionesDocente(Docente)
+
                     break;
                 case 8:
                     // Acciones con un alumno (inscribir, listar cursos, etc)
